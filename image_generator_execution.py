82fe68ae167c3f1b30ac034c1c836a85e095b42e
ReL1c113 <109995983+ReL1c113@users.noreply.github.com>
2024-10-17 12:30:11 +0530

    Add files via upload
    Added all necessary files

---
+++ image_generator_execution.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 11:30:06 2024
+
+@author: piyush.raj
+"""
+
+'Image Generator Website https://fal.ai/dashboard'
+
+import fal_client
+import os
+import webbrowser
+import pandas as pd
+
+class img_generator():
+    def __init__(self):
+        self.cred=pd.read_excel(r'FAL_KEY_cred.xlsx')
+        os.environ['FAL_KEY']=self.cred['FAL_KEY'][0] # My API KEY
+        if not os.getenv('FAL_KEY'):
+            raise Exception("FAL_KEY environment variable is not set!")    
+        print("FAL_KEY environment variable is set successfully.")
+        print(self.cred)
+        
+    def image_generator(self,prompt):
+        handler = fal_client.submit(
+            "fal-ai/fast-sdxl",
+            arguments={
+                "prompt": prompt
+            },
+        )
+        result = handler.get()
+        url=result['images'][0]['url']
+        return url
+        
+
+        
+        
+        
---
+++ streamlit_app_for_img_generation.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 12:10:26 2024
+
+@author: piyush.raj
+"""
+
+import streamlit as st
+from image_generator_execution import img_generator
+
+generator=img_generator()
+
+st.title("AI Image Generator")
+
+# Input field for the user's name
+prompt = st.text_input("Enter your Prompt:")
+
+# Check if the 'name' is submitted and store it in session state
+if prompt:
+    st.session_state["prompt"] = prompt
+    
+image_url = generator.image_generator(prompt)
+
+st.image(image_url, caption="This is an image from the web.", use_column_width=True)
